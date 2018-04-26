0977355a346cd8d959a38de721264338560c95de
youngvip <36694525+youngvip@users.noreply.github.com>
2018-04-26 21:31:49 +0300

    статья с коментами
    устал побеждать будстрап , шаблон статьи с коментами и формачкой сделал

--- veali/head.php
+++ veali/head.php
@@ -6,8 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">	
     <title>Мой блок</title> 	
-	<!-- Bootstrap CSS -->  
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	
+    
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/css/mdb.min.css" rel="stylesheet">
 </head>
 </html>
--- veali/newst.php
+++ veali/newst.php
@@ -1,26 +1,7 @@
 <body>
 	<div class="container-fluid">
-		<div class="row justify-content-center inset">
-			<div class="col-auto"><img src="images/inset-banner.jpg" class="img-fluid" alt="Сдесь должа быть картинка"></div>
-		</div>	
-		<div class="row justify-content-center menu-bar ">
-			<div class="col-md-auto d-lg-none d-xl-block">
-				<ul class="nav nav-pills" role="tablist">
-				  <li class="nav-item btn">
-					<a class="nav-link text-white"  href="/" role="tab">Главная</a>
-				  </li>
-				  <li class="nav-item btn">
-					<a class="nav-link text-white active" href="?page=new" role="tab">Новости</a>
-				  </li>
-				  <li class="nav-item btn">
-					<a class="nav-link text-white " href="?page=doc" role="tab">Документация</a>
-				  </li>				  
-				 
-				   
-				</ul>
-				<!-- Вкладка панели -->  
-			</div>			
-		</div>
+		
+		
 		
 		<div class="container">
 			<div class="row">
@@ -49,6 +30,114 @@
 											</div>																					 
 										</div>										 
 									</section>
+									<section>
+
+										<!--Main wrapper-->
+										<div class="comments-list text-center text-md-left mb-5">
+											<div class="text-center mb-4">
+												<h3>Комментарии<span class="badge blue">Три</span>
+												</h3>
+											</div>
+											<!--First row-->
+											<div class="row mb-4">
+												
+												
+												<!--Content column-->
+												<div class="col-sm-10 col-12">
+													<a>
+														<h4 class="font-weight-bold">Джон ДОУ</h4>
+													</a>
+													<div class="mt-2">
+														<ul class="list-unstyled">
+															<li class="comment-date">
+																<i class="fa fa-clock-o"></i> 05/10/2015</li>
+														</ul>
+													</div>
+													<p class="grey-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+													 консекват. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+													 nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+												</div>
+												<!--/.Content column-->
+											</div>
+											<!--/.First row-->
+											
+										</div>
+										<!--/.Main wrapper-->
+
+									</section>
+									<div class="col-12">
+									<section>
+
+            <!--Leave a reply form-->
+										<div class="reply-form text-center">
+											<h3 class="section-heading h3 pt-5">Оставить ответ</h3>
+											<p class="text-center mb-4">Пользователь Не Вошел В Систему</p>
+
+											<!--Grid row-->
+											<div class="row">
+
+												<!--Grid column-->
+												<div class="col-lg-4 col-md-12">
+
+													<div class="input-group md-form form-sm form-3 pl-0">
+														<div class="input-group-prepend">
+															<span class="input-group-text white grey-text" id="basic-addon9">1</span>
+														</div>
+														<input type="text" class="form-control form-control-sm mt-0 black-border rgba-white-strong" placeholder="Имя пользователя" aria-describedby="basic-addon9">
+													</div>
+
+												</div>
+												<!--Grid column-->
+
+												<!--Grid column-->
+												<div class="col-lg-4 col-md-6">
+
+													<div class="input-group md-form form-sm form-3 pl-0">
+														<div class="input-group-prepend">
+															<span class="input-group-text white grey-text" id="basic-addon10">2</span>
+														</div>
+														<input type="text" class="form-control form-control-sm mt-0 black-border rgba-white-strong" placeholder="Адрес эл. почты" aria-describedby="basic-addon10">
+													</div>
+
+												</div>
+												<!--Grid column-->
+
+												<!--Grid column-->
+												<div class="col-lg-4 col-md-6">
+
+													<div class="input-group md-form form-sm form-3 pl-0">
+														<div class="input-group-prepend">
+															<span class="input-group-text white grey-text" id="basic-addon101">3</span>
+														</div>
+														<input type="text" class="form-control form-control-sm mt-0 black-border rgba-white-strong" placeholder="Веб-сайт" aria-describedby="basic-1">
+													</div>
+
+												</div>
+												<!--Grid column-->
+
+											</div>
+											<!--Grid row-->
+
+											<!--Grid row-->
+											<div class="row">
+
+												<div class="col-12 mt-4">
+
+													<div class="form-group basic-textarea">
+														<textarea class="form-control rounded pl-3 pt-3" id="exampleFormControlTextarea1" rows="6" placeholder="Напиши что-нибудь здесь..."></textarea>
+													</div>
+
+													<div class="text-right">
+														<button class="btn btn-primary btn-md waves-effect waves-light"><ya-tr-span data-index="265-0" data-value="Submit" data-translation="Отправить" data-type="trSpan">Отправить</ya-tr-span></button>
+													</div>
+
+												</div>
+
+											</div>
+											<!--Grid row-->
+
+									</div></section>
+									</div>
 								</div>
 							</div>			
 						</div>
